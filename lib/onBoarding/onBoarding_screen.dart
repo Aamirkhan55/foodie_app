7e04d505afd946df283f61e4e70e10b34180713c
Amir55143 <aamirnoor55143@gmail.com>
2023-10-09 16:53:49 +0500

    OnBoarding

---
+++ lib/onBoarding/onBoarding_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:foodie_app/onBoarding/onBoarding_entity.dart';
+import 'package:foodie_app/theme/theme_style.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  List<OnBoardingEntity> onBoardingData = OnBoardingEntity.onBoardingData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: onBoardingData.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 80),
+              child: Column(
+                children: [
+                  Image.asset("assets/word_app_logo.png"),
+                  const SizedBox(height: 130),
+                  Image.asset("assets/${onBoardingData[index].image}"),
+                  const SizedBox(height: 40),
+                  Text(
+                    "${onBoardingData[index].title}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "${onBoardingData[index].description}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: index == 0 ? primaryColorED6E1B :Colors.grey,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: index == 1 ? primaryColorED6E1B :Colors.grey,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                         const SizedBox(height: 10),
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: index == 2 ? primaryColorED6E1B :Colors.grey,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ],
+                    )
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
