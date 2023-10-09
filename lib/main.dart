beb3668856b3f6aed830da1943c4e65e705e82d7
Amir55143 <aamirnoor55143@gmail.com>
2023-10-09 16:14:33 +0500

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:foodie_app/screen/main_screen.dart';
+import 'package:foodie_app/screen/main/main_screen.dart';
+import 'package:foodie_app/screen/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: primarySwatch,
         ),
-        home: const MainScreen(),
+        home: const SplashScreen(child: MainScreen()),
       );
   }
 }
