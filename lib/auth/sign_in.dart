3a5d79b493bcf2db5835f3b3a60bc22e420b23a5
Amir55143 <aamirnoor55143@gmail.com>
2023-10-10 15:34:02 +0500

    SignUp/Screen

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -22,135 +22,141 @@ class _SignInPageState extends State<SignInPage> {
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 100),
-            const Text(
-              'Sign In',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 100),
+                  const Text(
+                    'Sign In',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  const FormContainerWidget(hintText: 'Email or Username'),
+                  const SizedBox(height: 40),
+                  const FormContainerWidget(hintText: 'Password'),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                              value: _rememberMeCheckValue,
+                              onChanged: (value) {
+                                setState(() {
+                                  _rememberMeCheckValue = value!;
+                                });
+                              }),
+                          const Text(
+                            'Remember me',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        'Forgot Password',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: primaryColorED6E1B,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 80),
+                  ButtonContainerWidget(
+                    title: 'Sign In',
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PremiumScreen()),
+                          (route) => false);
+                    },
+                  ),
+                  const SizedBox(height: 60),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: Colors.black,
+                      )),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text('or'),
+                      ),
+                      Expanded(
+                          child: Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: Colors.black,
+                      )),
+                    ],
+                  ),
+                  const SizedBox(height: 60),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _signInOption(
+                        icon: FontAwesomeIcons.google,
+                        color: redColor,
+                      ),
+                      const SizedBox(width: 10),
+                      _signInOption(
+                        icon: FontAwesomeIcons.facebook,
+                        color: Colors.blue.shade600,
+                      ),
+                      const SizedBox(width: 10),
+                      _signInOption(
+                        icon: FontAwesomeIcons.linkedin,
+                        color: Colors.blue.shade300,
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            const FormContainerWidget(hintText: 'Email or Username'),
-            const SizedBox(height: 20),
-            const FormContainerWidget(hintText: 'Password'),
-            const SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Checkbox(
-                        value: _rememberMeCheckValue,
-                        onChanged: (value) {
-                          setState(() {
-                            _rememberMeCheckValue = value!;
-                          });
-                        }),
-                    const Text(
-                      'Remember me',
-                      style: TextStyle(
-                        fontSize: 15,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account ?",
+                        style: TextStyle(fontSize: 15),
                       ),
-                    ),
-                  ],
-                ),
-                const Text(
-                  'Forgot Password',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: primaryColorED6E1B,
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            ButtonContainerWidget(
-              title: 'Sign In',
-              onTap: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const PremiumScreen()),
-                    (route) => false);
-              },
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                    child: Container(
-                  width: double.infinity,
-                  height: 1,
-                  color: Colors.black,
-                )),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text('or'),
-                ),
-                Expanded(
-                    child: Container(
-                  width: double.infinity,
-                  height: 1,
-                  color: Colors.black,
-                )),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _signInOption(
-                  icon: FontAwesomeIcons.google,
-                  color: redColor,
-                ),
-                const SizedBox(width: 10),
-                _signInOption(
-                  icon: FontAwesomeIcons.facebook,
-                  color: Colors.blue.shade600,
-                ),
-                const SizedBox(width: 10),
-                _signInOption(
-                  icon: FontAwesomeIcons.linkedin,
-                  color: Colors.blue.shade300,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have an account ?",
-                  style: TextStyle(fontSize: 15),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUpPage()),
-                        (route) => false);
-                  },
-                  child: const Text(
-                    'Create Account',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: primaryColorED6E1B,
-                    ),
-                  ),
-                )
-              ],
-            )
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpPage()),
+                              (route) => false);
+                        },
+                        child: const Text(
+                          'Create Account',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: primaryColorED6E1B,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
           ],
         ),
       ),
     );
   }
 
-  _signInOption({required IconData icon, required Color color}) {
-    Container(
+ Widget _signInOption({required IconData icon, required Color color}) {
+    return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10.0),
       width: 40,
       height: 40,
